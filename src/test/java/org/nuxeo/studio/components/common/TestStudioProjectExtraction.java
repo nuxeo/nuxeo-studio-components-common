e01e9cf728b04e112f6acd3e88c6a2e584cee8c6
Yannis JULIENNE <yjulienne@nuxeo.com>
2021-05-19 14:46:35 +0200

    NXS-6312: Handle workflows extraction

--- src/test/java/org/nuxeo/studio/components/common/TestStudioProjectExtraction.java
+++ src/test/java/org/nuxeo/studio/components/common/TestStudioProjectExtraction.java
@@ -9,6 +9,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.nuxeo.studio.components.common.bundle.ContributionsHolder;
 import org.nuxeo.studio.components.common.mapper.descriptors.SchemaBindingDescriptor;
+import org.nuxeo.studio.components.common.mapper.descriptors.WorkflowDescriptor;
 
 public class TestStudioProjectExtraction extends AbstractExtractorTest {
     public static final String STUDIO_EXTENSTIONS_FILE = "studio-extensions.xml";
@@ -23,7 +24,10 @@ public class TestStudioProjectExtraction extends AbstractExtractorTest {
     @Test
     public void testLoad() throws URISyntaxException {
         ContributionsHolder holder = loadComponent(STUDIO_EXTENSTIONS_FILE);
-        List<SchemaBindingDescriptor> contributions = holder.getContributions(SchemaBindingDescriptor.class);
-        assertThat(contributions).hasSize(13);
+        List<SchemaBindingDescriptor> schemaContribs = holder.getContributions(SchemaBindingDescriptor.class);
+        assertThat(schemaContribs).hasSize(13);
+
+        List<WorkflowDescriptor> workflowContribs = holder.getContributions(WorkflowDescriptor.class);
+        assertThat(workflowContribs).hasSize(3);
     }
 }
