df7ac26dada082e3a4eec1289a80aba772a12cb4
Florian <fbematol@nuxeo.com>
2021-11-24 17:10:56 +0100

    NXS-6580: Enable resources contributions loading

--- src/test/java/org/nuxeo/studio/components/common/TestContributionsLoader.java
+++ src/test/java/org/nuxeo/studio/components/common/TestContributionsLoader.java
@@ -27,7 +27,11 @@ import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.nuxeo.studio.components.common.bundle.ContributionsHolder;
+import org.nuxeo.studio.components.common.mapper.descriptors.CSVResourceDescriptor;
+import org.nuxeo.studio.components.common.mapper.descriptors.I18nResourceDescriptor;
+import org.nuxeo.studio.components.common.mapper.descriptors.ImageResourceDescriptor;
 import org.nuxeo.studio.components.common.mapper.descriptors.OperationDescriptor;
+import org.nuxeo.studio.components.common.mapper.descriptors.XSDResourceDescriptor;
 
 public class TestContributionsLoader extends AbstractExtractorTest {
     public static final String JAR_FILE = "src/test/resources/test-project-core-1.0-SNAPSHOT.jar";
@@ -52,12 +56,27 @@ public class TestContributionsLoader extends AbstractExtractorTest {
         new ContributionsLoader(holder, opts).loadFromJarFile(new File(JAR_FILE).toURI());
         List<OperationDescriptor> contributions = holder.getContributions(OperationDescriptor.class);
         assertThat(contributions).hasSize(1);
+        List<ImageResourceDescriptor> imgResourceDescriptors = holder.getContributions(ImageResourceDescriptor.class);
+        assertThat(imgResourceDescriptors).hasSize(1);
     }
 
     @Test
     public void loadFromDirectory() {
         new ContributionsLoader(holder, opts).loadFromDirectory("src/test/resources");
+
         List<OperationDescriptor> contributions = holder.getContributions(OperationDescriptor.class);
         assertThat(contributions).hasSize(1);
+
+        List<ImageResourceDescriptor> imgResourceDescriptors = holder.getContributions(ImageResourceDescriptor.class);
+        assertThat(imgResourceDescriptors).hasSize(4);
+
+        List<CSVResourceDescriptor> csvResourceDescriptors = holder.getContributions(CSVResourceDescriptor.class);
+        assertThat(csvResourceDescriptors).hasSize(4);
+
+        List<XSDResourceDescriptor> xsdResourceDescriptors = holder.getContributions(XSDResourceDescriptor.class);
+        assertThat(xsdResourceDescriptors).hasSize(4);
+
+        List<I18nResourceDescriptor> i18nResourceDescriptors = holder.getContributions(I18nResourceDescriptor.class);
+        assertThat(i18nResourceDescriptors).hasSize(4);
     }
 }
